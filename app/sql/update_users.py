c4512c4b3400e0eed5d723f978d4d9f1f0777f97
EmmaBYPeng <u3502350@connect.hku.hk>
2016-07-17 15:35:52 -0700

    Load database information from config file

--- app/sql/update_users.py
+++ app/sql/update_users.py
@@ -1,16 +1,34 @@
 import datetime
+import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from init_users import *
 
-engine = create_engine('mysql://root:153399345@localhost/deep_annotator', echo=True)
+# Contact admin if config files don't exist
+CONFIG = '../../configs/config.json'
+USER_CONFIG = '../../configs/users.json'
 
-# create a Session
+
+def read_json(data_path):
+  with open(data_path, 'r') as fp:
+    return json.load(fp)
+
+# Connect to a database
+config = read_json(CONFIG)
+db = config['database']
+db_password = config['db_password']
+mysql = 'mysql://root:'+db_password+'@localhost/'+db
+engine = create_engine(mysql, echo=True)
+
+# Create a Session
 Session = sessionmaker(bind=engine)
 session = Session()
 
-user = User("admin","admin")
-session.add(user)
+# Insert users to table
+users = read_json(USER_CONFIG)
+for username, password in users.iteritems():
+    user = User(username, password)
+    session.add(user)
 
 # commit the record of the database
 session.commit()
--- misc/create_config.py
+++ misc/create_config.py
@@ -1,5 +1,6 @@
 import json
 import argparse
+import os
 
 
 def write_json(data, data_path):
@@ -12,19 +13,40 @@ def read_json(data_path):
     return json.load(fp)
 
 
+def create_user_config(users):
+    while True:
+        try:
+            username = raw_input("Username: ")
+            password = raw_input("Password: ")
+            users[username] = password
+        except EOFError:
+            break
+
+
 def main(params):
+  if not os.path.exists(params['config_root']):
+      os.makedirs(params['config_root'])
+
   config = {}
   config['video_root'] = params['video_root']
   config['server'] = params['server']
   config['database'] = params['database']
-  write_json(config, 'config.json')
+  config['db_password'] = raw_input("Database password: ")
+  config_path = os.path.join(params['config_root'], 'config.json')
+  write_json(config, config_path)
+
+  users = {}
+  user_config_path = os.path.join(params['config_root'], 'users.json')
+  create_user_config(users)
+  write_json(users, user_config_path)
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument('-video_root', nargs='?', default='../videos', type=str)
+  parser.add_argument('-video_root', nargs='?', default='../public/static/video', type=str)
   parser.add_argument('-server', nargs='?', default='128.12.137.178', type=str)
   parser.add_argument('-database', nargs='?', default='deep_annotator', type=str)
+  parser.add_argument('-config_root', nargs='?', default='../configs/', type=str)
 
   args = parser.parse_args()
   params = vars(args)
