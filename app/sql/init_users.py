3b42b51a31b0488ec3a6c647b72e76c5ed4a8048
EmmaBYPeng <u3502350@connect.hku.hk>
2016-07-17 15:40:04 -0700

    More config

--- app/sql/init_users.py
+++ app/sql/init_users.py
@@ -3,8 +3,25 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
+import json
 
-engine = create_engine('mysql://root:153399345@localhost/deep_annotator', echo=True)
+
+# Contact admin if config files don't exist
+CONFIG = '../../configs/config.json'
+
+
+def read_json(data_path):
+  with open(data_path, 'r') as fp:
+    return json.load(fp)
+
+
+# Connect to a database
+config = read_json(CONFIG)
+db = config['database']
+db_password = config['db_password']
+mysql = 'mysql://root:'+db_password+'@localhost/'+db
+engine = create_engine(mysql, echo=True)
+
 Base = declarative_base()
 
 ########################################################################
