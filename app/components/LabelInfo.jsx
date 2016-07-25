002b89110b5685caf1b222fb666f030664a16a55
dingnigefei <luozl1993@gmail.com>
2016-07-25 03:30:52 -0700

    added some event handlers...

--- app/components/LabelInfo.jsx
+++ app/components/LabelInfo.jsx
@@ -8,21 +8,57 @@ export default class LabelInfo extends React.Component {
     this.state = {
       name: "frame label 1"
     };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    self = this;
+
+    self.props.updateOption(self.props.id, event.target.value);
   }
 
   render() {
     var self = this;
 
-    return (
-      <div className={(self.props.isFrameLabels ? "frame" : "object")+"-label-info"}>
-        {(self.props.isFrameLabels ? "Frame" : "Object")}
-      </div>
-    );
+    if (self.props.isFrameLabels) {
+      // three cases: visible, outside of view frame, occluded or obstructed
+      return (
+        <div className="label-info frame-label-info">
+          <h5>Frame</h5>
+          <div className="radio">
+            <label>
+              <input type="radio" name="optradio" value={0} onChange={self.handleChange} />
+              Visible
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" name="optradio" value={1} onChange={self.handleChange} />
+              Outside of frame
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input type="radio" name="optradio" value={2} onChange={self.handleChange} />
+              Occluded
+            </label>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="label-info object-label-info">
+          <h5>Object</h5>
+        </div>
+      );
+    }
   }
 }
 
 LabelInfo.propTypes = {
-  isFrameLabels: React.PropTypes.bool.isRequired // true: frame, false: object
+  isFrameLabels: React.PropTypes.bool.isRequired, // true: frame, false: object
+  id: React.PropTypes.number.isRequired
 };
 
 LabelInfo.defaultProps = {
