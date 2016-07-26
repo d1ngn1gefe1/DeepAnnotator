f253d70754a834b17c0df0e583d8f5f36746e26d
EmmaBYPeng <u3502350@connect.hku.hk>
2016-07-26 15:30:54 -0700

    Final fix for font-awesome webpack (hopefully)

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,8 @@ var config = {
         loader: "style!css!sass"
       },
       {
-        test: /\.png|\.jpe?g|\.gif|\.svg|\.woff|\.woff2|\.ttf|\.eot|\.ico|\.svg$/,
-        loader: 'file?name=static/js/fonts/[name].[hash].[ext]?'
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url?limit=100000@name=[name][ext]&name=./[hash].[ext]"
       },
       {
         test: /\.json$/,
