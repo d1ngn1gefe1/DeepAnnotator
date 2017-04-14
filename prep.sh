9a4cf816619dc3d6043bb4c012749701d88f3ef5
dingnigefei <luozl1993@gmail.com>
2017-04-13 23:11:19 -0700

    pred.sh with relative path

--- prep.sh
+++ prep.sh
@@ -1,9 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+normal=$(tput sgr0)
+green=$(tput setaf 2)
 #sudo pip install ffmpeg
 #sudo apt-get update
 #sudo apt-get install apache2
 #sudo pip install Flask-SQLAlchemy
 #sudo apt-get install nodejs
 #sudo apt-get install npm
-python misc/prep_data.py --data_dir "/home/alan/bigdata/thermset" --frame_per_second 2
-python misc/create_config.py -server 128.12.137.178 -config_root '/home/www/DeepAnnotator/app/sql/configs/'
+echo "\n${green}pred_data...${normal}"
+cd ./misc
+# python prep_data.py --data_dir '/home/alan/bigdata/thermset'
+echo "\n${green}create_config...${normal}"
+# python create_config.py --server 128.12.137.178 --config_root '../app/sql/configs/'
+echo "\n${green}update_users...${normal}"
+cd ../app/sql
+python update_users.py
+echo "\n${green}create_config...${normal}"
+python update_options.py
