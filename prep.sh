a32dfac48f44b229851df8c28fc6506c7ef3b5b2
Alisha <alisha.rege@gmail.com>
2017-04-17 17:18:37 -0700

    small change

--- prep.sh
+++ prep.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 normal=$(tput sgr0)
 green=$(tput setaf 2)
-#sudo pip install ffmpeg
-#sudo apt-get update
-#sudo apt-get install apache2
-#sudo pip install Flask-SQLAlchemy
-#sudo apt-get install nodejs
-#sudo apt-get install npm
+sudo pip install ffmpeg
+sudo apt-get update
+sudo apt-get install apache2
+sudo pip install Flask-SQLAlchemy
+sudo apt-get install nodejs
+sudo apt-get install npm
 echo "\n${green}pred_data...${normal}"
 cd ./misc
-python3 prep_data.py --input_path '/home/alan/bigdata/thermset' --output_path '../public/static/video'
+python3 prep_data.py --input_path '/home/data/thermset' --output_path '../public/static/video'
 echo "\n${green}create_config...${normal}"
-python3 create_config.py --server 128.12.137.178 --config_root '../app/sql/configs/'
+python3 create_config.py --server aicare.stanford.edu --config_root '../app/sql/configs/'
 echo "\n${green}update_users...${normal}"
 cd ../app/sql
 python3 update_users.py
