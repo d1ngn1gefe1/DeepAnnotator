a24953fb41a77d5f352e884ebbc29811c054fe9e
Alisha Rege <alisha.rege@gmail.com>
2017-04-19 00:53:29 -0700

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,19 +1,19 @@
 #/bin/bash
 normal=$(tput sgr0)
 green=$(tput setaf 2)
-#sudo pip install ffmpeg
-#sudo apt-get update
-#sudo apt-get install apache2
-#sudo pip install Flask-SQLAlchemy
-#sudo apt-get install nodejs
-#sudo apt-get install npm
-#echo "\n${green}pred_data...${normal}"
+sudo pip install ffmpeg
+sudo apt-get update
+sudo apt-get install apache2
+sudo pip install Flask-SQLAlchemy
+sudo apt-get install nodejs
+sudo apt-get install npm
+echo "\n${green}pred_data...${normal}"
 cd ./misc
 python3 prep_data.py --input_path '/data/try2' --output_path '../public/static/video'
 echo "\n${green}create_config...${normal}"
-#python3 create_config.py --server aicare.stanford.edu --config_root '../app/sql/configs/'
-#echo "\n${green}update_users...${normal}"
-#cd ../app/sql
-#python update_users.py
-#echo "\n${green}update_options...${normal}"
-#python update_options.py
+python3 create_config.py --server aicare.stanford.edu --config_root '../app/sql/configs/'
+echo "\n${green}update_users...${normal}"
+cd ../app/sql
+python update_users.py
+echo "\n${green}update_options...${normal}"
+python update_options.py
